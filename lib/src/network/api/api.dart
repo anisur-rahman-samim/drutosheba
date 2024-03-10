2e5fd747c81a6de67d772572b270908821e4c492
abdurrakibrafi <abdurrakibrafi469@gmail.com>
2024-03-10 11:53:50 +0600

    setup done

---
+++ lib/src/network/api/api.dart
@@ -0,0 +1,58 @@
+class Api{
+  /// base url
+  static const domain = "https://carbook.cutiaidcorporation.com";
+  static const baseUrl = "$domain/api";
+  static String getImageURL(endPoint) => '$domain/$endPoint';
+
+
+  ///Auth
+  static const login = "$baseUrl/partner/login";
+  static const register = "$baseUrl/partner/register";
+  static const verify = "$baseUrl/partner/verify";
+  static const resend = "$baseUrl/partner/resend-otp";
+  static const forgot = "$baseUrl/partner/forgot-password";
+  static const resendForgot = "$baseUrl/partner/resend-forgot";
+  static const forgotVerify = "$baseUrl/partner/forgot-verify";
+  static const profile = "$baseUrl/partner/profile";
+  static const profileUpdate = "$baseUrl/partner/profile-update";
+
+  ///Rental Trip
+  static const tripRequest = "$baseUrl/partner/trip-request";
+  static const tripCancel = "$baseUrl/partner/trip-cancel";
+  static const tripHistory = "$baseUrl/partner/penalty-history";
+  static const tripStarted = "$baseUrl/partner/trip-started";
+  static const tripCompleted = "$baseUrl/partner/trip-completed";
+  static const tripBidSubmit = "$baseUrl/partner/trip-bid/submit";
+  static const waitingBid = "$baseUrl/partner/waiting-bid";
+  static const tripDetails = "$baseUrl/partner/trip-details";
+
+  ///Driver
+  static const driverManage = "$baseUrl/partner/driver-manage";
+  static const driverAdd = "$baseUrl/partner/driver-add";
+
+  ///Penalty and Balance
+  static const credit = "$baseUrl/partner/credit";
+  static const creditHistory = "$baseUrl/partner/credit-history";
+  static const dashboard = "$baseUrl/partner/dashboard";
+  static const leaderboard = "$baseUrl/partner/leaderboard";
+  static const withdrawalSubmit = "$baseUrl/partner/withdrawal-submit";
+  static const creditBuy = "$baseUrl/partner/credit-buy";
+
+  ///Return Trip
+  static const returnTripSubmit = "$baseUrl/partner/return-trip-submit";
+  static const returnTripBids = "$baseUrl/partner/return-trip-bids";
+  static const returnTripConfirm = "$baseUrl/partner/return-trip-confirm";
+
+  ///Vehicle
+  static const vehicles = "$baseUrl/partner/vehicles";
+  static const vehicleManage = "$baseUrl/partner/vehicle-manage";
+  static const fuelTypes = "$baseUrl/partner/fueltypes";
+  static const vehicleAdd = "$baseUrl/partner/vehicle-add";
+  static const returnTripCompleted = "$baseUrl/partner/return-trip-completed";
+
+  ///common
+  static const districts = "$baseUrl/partner/districts";
+  static const shareReferKey = "$baseUrl/partner/share-referkey";
+  static const offer = "$baseUrl/partner/offer";
+  static const help = "$baseUrl/partner/help";
+}
---
+++ lib/src/network/base_client/base_client.dart
@@ -0,0 +1,164 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:druto_seba_driver/src/services/local_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import '../../configs/app_texts.dart';
+
+
+
+class BaseClient {
+
+  static getRequest({required String api, params}) async {
+
+
+    debugPrint("\nYou hit: $api");
+    debugPrint("Request Params: $params");
+
+    var headers = {
+      'Content-Type': 'application/json; charset=UTF-8',
+      'Accept': 'application/json',
+      "remember_token": "${LocalStorage.getData(key: AppTexts.token)}"
+    };
+    debugPrint("statusCode: remember_token: ${LocalStorage.getData(key: AppTexts.token)}");
+
+    http.Response response = await http.get(
+      Uri.parse(api).replace(queryParameters: params),
+      headers: headers,
+    );
+    return response;
+  }
+
+  static postRequest({required String api, body}) async {
+
+    debugPrint('\nYou hit: $api');
+    debugPrint('Request Body: ${jsonEncode(body)}');
+
+    var headers = {
+      'Accept': 'application/json',
+      // 'Content-Type': 'application/json; charset=UTF-8',
+      "remember_token": "${LocalStorage.getData(key: AppTexts.token)}"
+    };
+
+    http.Response response = await http.post(
+      Uri.parse(api),
+      body: body,
+      headers: headers,
+    );
+    return response;
+  }
+  static deleteRequest({required String api, body}) async {
+
+    debugPrint('\nYou hit: $api');
+    debugPrint('Request Body: ${jsonEncode(body)}');
+
+    var headers = {
+      'Accept': 'application/json',
+      "remember_token":  "${LocalStorage.getData(key: AppTexts.token)}"
+    };
+
+
+    http.Response response = await http.delete(
+      Uri.parse(api),
+      body: body,
+      headers: headers,
+    );
+    return response;
+  }
+
+  static multipartAddRequest({
+    required String api,
+    required Map<String, String> body,
+    required String fileKeyName,
+    required String filePath,
+  }) async {
+
+
+    debugPrint("\nYou hit: $api");
+    debugPrint("Request body: $body");
+
+    var headers = {
+      'Accept': 'application/json',
+      "remember_token": "${LocalStorage.getData(key: AppTexts.token)}"
+    };
+
+    http.MultipartRequest request;
+    if (filePath.isEmpty || filePath == '') {
+      request = http.MultipartRequest('POST', Uri.parse(api))
+        ..fields.addAll(body)
+        ..headers.addAll(headers);
+    } else {
+      request = http.MultipartRequest('POST', Uri.parse(api))
+        ..fields.addAll(body)
+        ..headers.addAll(headers)
+        ..files.add(await http.MultipartFile.fromPath(fileKeyName, filePath));
+    }
+
+    var streamedResponse = await request.send();
+    var response = await http.Response.fromStream(streamedResponse);
+
+    return response;
+  }
+
+  static handleResponse(http.Response response) async {
+    try {
+
+
+      if (response.statusCode >= 200 && response.statusCode <= 210) {
+        debugPrint('SuccessCode: ${response.statusCode}');
+        debugPrint('SuccessResponse: ${response.body}');
+
+        if (response.body.isNotEmpty) {
+          return json.decode(response.body);
+        } else {
+          return response.body;
+        }
+      } else if (response.statusCode == 401) {
+        debugPrint("statusCode: 401");
+        // _logout();
+        String msg = "Unauthorized";
+        if (response.body.isNotEmpty) {
+          if(json.decode(response.body)['errors'] != null){
+            msg = json.decode(response.body)['errors'];
+          }
+        }
+        throw msg;
+      } else if (response.statusCode == 404) {
+        debugPrint("statusCode: 404");
+        throw 'Page Not Found!';
+      } else if (response.statusCode == 500) {
+        debugPrint("statusCode: 500");
+        throw "Server Error";
+      } else {
+        debugPrint('ErrorCode: ${response.statusCode}');
+        debugPrint('ErrorResponse: ${response.body}');
+
+        String msg = "Something went wrong";
+        if (response.body.isNotEmpty) {
+          var data = jsonDecode(response.body)['errors'];
+          if(data == null){
+            msg = jsonDecode(response.body)['message'] ?? msg;
+          }
+          else if (data is String) {
+            msg = data;
+          } else if (data is Map) {
+            msg = data['email'][0];
+          }
+        }
+
+        throw msg;
+      }
+    } on SocketException catch (_) {
+
+    } on FormatException catch (_) {
+      throw "Bad response format";
+    } catch (e) {
+      throw e.toString();
+    }
+  }
+
+/*static void _logout() {
+    LocalStorage.removeData(key: LocalStorageKey.token);
+    Get.offAll(() => const LoginScreen());
+  }*/
+}
